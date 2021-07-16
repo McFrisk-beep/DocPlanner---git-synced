d41c5e240695e4c834bc2a69ef6eef499b945dd7
Raphael Baligod <raphael.baligod@yahoo.com>
2021-07-16 18:52:51 +0800

    Added option for manual processing

--- src/Atstratus - HAYS - DupResoMR.js
+++ src/Atstratus - HAYS - DupResoMR.js
@@ -20,34 +20,69 @@ function(record, search, runtime, https, config) {
      * @since 2015.1
      */
     function getInputData() {
-        var customrecord_hays_eventSearchObj = search.create({
-            type: "customrecord_hays_event",
-            filters:
-            [
-               ["custrecord_hays_event_errorcodelist","contains","CANT_MODIFY_SUB"], 
-               "AND", 
-               ["custrecord_hays_event_status","anyof","1","3"], 
-               "AND", 
-               ["custrecord_hays_event_errorcodelist","contains","CANT_MODIFY_SUB"], 
-               "AND", 
-               ["internalid","anyof","589421"]  //Remove soon. This is just a sample.
-            ],
-            columns:
-            [
-               search.createColumn({
-                  name: "id",
-                  sort: search.Sort.ASC,
-                  label: "ID"
-               }),
-               search.createColumn({name: "created", label: "Date Created"}),
-               search.createColumn({name: "custrecord_hays_event_subsidiary", label: "Subsidiary"}),
-               search.createColumn({name: "custrecord_hays_event_recordtype", label: "Record Type"}),
-               search.createColumn({name: "custrecord_hays_event_status", label: "Status"}),
-               search.createColumn({name: "custrecord_hays_event_errorcodelist", label: "Error Code List"})
-            ]
+        var haysEventID = currentScript.getParameter({
+            name: 'custscript_haysevent_id'
         });
-        var searchResultCount = customrecord_hays_eventSearchObj.runPaged().count;
-        log.audit("customrecord_hays_eventSearchObj result count",searchResultCount);
+        var customrecord_hays_eventSearchObj;
+
+        if(haysEventID){
+            customrecord_hays_eventSearchObj = search.create({
+                type: "customrecord_hays_event",
+                filters:
+                [
+                   ["custrecord_hays_event_errorcodelist","contains","CANT_MODIFY_SUB"], 
+                   "AND", 
+                   ["custrecord_hays_event_status","anyof","3"], 
+                   "AND", 
+                   ["custrecord_hays_event_errorcodelist","contains","CANT_MODIFY_SUB"], 
+                   "AND", 
+                   ["internalid","anyof",haysEventID]   //For manual processing
+                ],
+                columns:
+                [
+                   search.createColumn({
+                      name: "id",
+                      sort: search.Sort.ASC,
+                      label: "ID"
+                   }),
+                   search.createColumn({name: "created", label: "Date Created"}),
+                   search.createColumn({name: "custrecord_hays_event_subsidiary", label: "Subsidiary"}),
+                   search.createColumn({name: "custrecord_hays_event_recordtype", label: "Record Type"}),
+                   search.createColumn({name: "custrecord_hays_event_status", label: "Status"}),
+                   search.createColumn({name: "custrecord_hays_event_errorcodelist", label: "Error Code List"})
+                ]
+            });
+            var searchResultCount = customrecord_hays_eventSearchObj.runPaged().count;
+            log.audit("Manual Processing - START", 'To process: ' + searchResultCount);
+        }
+        else{
+            customrecord_hays_eventSearchObj = search.create({
+                type: "customrecord_hays_event",
+                filters:
+                [
+                   ["custrecord_hays_event_errorcodelist","contains","CANT_MODIFY_SUB"], 
+                   "AND", 
+                   ["custrecord_hays_event_status","anyof","3"], 
+                   "AND", 
+                   ["custrecord_hays_event_errorcodelist","contains","CANT_MODIFY_SUB"]
+                ],
+                columns:
+                [
+                   search.createColumn({
+                      name: "id",
+                      sort: search.Sort.ASC,
+                      label: "ID"
+                   }),
+                   search.createColumn({name: "created", label: "Date Created"}),
+                   search.createColumn({name: "custrecord_hays_event_subsidiary", label: "Subsidiary"}),
+                   search.createColumn({name: "custrecord_hays_event_recordtype", label: "Record Type"}),
+                   search.createColumn({name: "custrecord_hays_event_status", label: "Status"}),
+                   search.createColumn({name: "custrecord_hays_event_errorcodelist", label: "Error Code List"})
+                ]
+            });
+            var searchResultCount = customrecord_hays_eventSearchObj.runPaged().count;
+            log.audit("Automatic Processing - START", 'To process: ' + searchResultCount);
+        }
 
         return customrecord_hays_eventSearchObj;
     }
